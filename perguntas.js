7f2d1beabe60de62c2f6eb6140bfd1e50de00619
esthef4nysouza <0000111507653xsp@al.educacao.sp.gov.br>
2024-12-04 16:16:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como "Jango"?',
+    'João Goulart.'
+)
+
+criaCartao(
+    'Biologia',
+    'O pólen produzido por abelhas é usado para...',
+    'É utilizado na alimentação humana e de animais como fonte suplementar de energia e nutrientes.'
+)
+
+criaCartao(
+    'Matemática',
+    'No meu jardim existe 3 pés de alface, 1 de pepino e 5 de cenoura. Quantos pés eu tenho no total?',
+    'Eu, como a maior parte das pessoas, tenho 2 pés.'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Qual o coletivo de "camelos"?',
+    'Cáfila.'
+)
